54c4f0a77f81599f8e42999399c49ea0aa6fc932
Rehab <rahmed@panarab-media.com>
2021-02-14 20:42:03 +0200

    feat: add card, carditem, and footer components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home'
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp'
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home}></Route>
+          <Route path='/services' exact component={Services}></Route>
+          <Route path='/products' exact component={Products}></Route>
+          <Route path='/sign-up' exact component={SignUp}></Route>
         </Switch>
 
       </Router>
